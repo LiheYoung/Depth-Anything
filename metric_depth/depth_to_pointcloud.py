632eb64cc5b39ebe91222e8e69740e82dfdc3542
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2024-02-01 01:54:13 +1100

    Create depth_to_pointcloud.py

---
+++ metric_depth/depth_to_pointcloud.py
@@ -0,0 +1,4 @@
+# Born out of Issue 36. 
+# Allows  the user to set up own test files to infer on (Create a folder my_test and add subfolder input and output in the metric_depth directory before running this script.)
+# Make sure you have the necessary libraries
+
